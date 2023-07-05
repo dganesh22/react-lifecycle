2b0d1c210ef757eba377b8d7e02504373bd9403f
dganesh22 <dganesh2022@gmail.com>
2023-07-05 11:51:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+      super(props)
+    }
+
+    render() {
+      return (
+        <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                  <h3 className="display-1 text-success">React Lifecycle</h3>
+              </div>
+            </div>
+
+            <div className="row">
+                <div className="col-md-12">
+                    <button className="btn btn-outline-success">Mount</button>
+                    
+                    <button className="btn btn-outline-danger float-end">UnMount</button>
+                </div>
+            </div>
+        </div>
+      )
+    }
 }
 
-export default App;
+export default App
